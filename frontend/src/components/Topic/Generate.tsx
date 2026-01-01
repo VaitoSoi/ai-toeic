66ec63a563958f00c6d13ecfa22824f9485586ee
VaitoSoi <duyduy15456@gmail.com>
2026-01-01 22:57:19 +0700

    chore(fe): move "generating topic" screen to submit page

--- frontend/src/components/Topic/Generate.tsx
+++ frontend/src/components/Topic/Generate.tsx
@@ -1,7 +1,6 @@
-import { ChevronLeft, ChevronRight, Mail, NotebookText, Sparkle } from "lucide-react";
+import { ChevronLeft, ChevronRight, Mail, NotebookText } from "lucide-react";
 import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import { BarLoader } from "react-spinners";
 import { error } from "../Toast";
 import api from "@/lib/api";
 import type { Topic } from "@/lib/typing";
@@ -26,69 +25,53 @@ function Generate() {
     }, [part, navigator]);
     useEffect(() => part == 2 || part == 3 ? void generateTopic() : undefined, [part, generateTopic]);
 
-    return <div className="h-full w-full flex">{
-        part
-            ? <div className="m-auto">
-                <div className="flex flex-col items-center gap-5">
-                    <div className="relative">
-                        <div className="absolute inset-0 bg-blue-100 rounded-full animate-ping opacity-75"></div>
-                        <div className="relative bg-white p-6 rounded-full shadow-lg border ">
-                            <Sparkle className="w-10 h-10 text-blue-500 animate-pulse" />
-                        </div>
-                    </div>
-                    <div className="flex flex-col items-center">
-                        <h1 className="text-3xl font-bold">Generating topic</h1>
-                        <p className="px-10 text-center lg:p-0 text-xl">The AI is crafting a unique prompt based on TOEIC standards</p>
-                    </div>
-                    <BarLoader width={300} />
-                </div>
+    return <div className="h-full w-full flex">
+        <div className="m-auto w-150">
+            <div
+                className="w-fit flex py-3 px-3 lg:px-0 flex-row items-center text-slate-400 hover:text-slate-800 cursor-pointer transition-all duration-200"
+                onClick={() => navigator("/", { viewTransition: true })}
+            >
+                <ChevronLeft className="size-7" />
+                <p className="text-lg">Go back to dashboard</p>
             </div>
-            : <div className="m-auto w-150">
-                <div
-                    className="w-fit flex py-3 px-3 lg:px-0 flex-row items-center text-slate-400 hover:text-slate-800 cursor-pointer transition-all duration-200"
-                    onClick={() => navigator("/", { viewTransition: true })}
-                >
-                    <ChevronLeft className="size-7" />
-                    <p className="text-lg">Go back to dashboard</p>
-                </div>
-                <div className="lg:border-2 lg:rounded-md p-5">
-                    <h1 className="text-2xl font-bold">Pick a part</h1>
-                    <h2 className="text-xl font-normal text-slate-400">Select the TOEIC writing part you want to practice:</h2>
-                    <div className="w-full mt-4 flex flex-col gap-2">
-                        <div
-                            className="w-full p-5 flex flex-row items-center gap-5 border-2 rounded-md hover:bg-green-300/20 hover:border-green-300 transition-all duration-200 group cursor-pointer"
-                            onClick={() => setPart(2)}
-                        >
-                            <div className="p-3 rounded-full bg-green-100 text-green-600 group-hover:bg-green-200 transition-colors">
-                                <Mail className="size-10" />
-                            </div>
-                            <div>
-                                <h1 className="text-xl font-bold group-hover:text-green-800 transition-all duration-200">Part 2</h1>
-                                <h2 className="text-lg font-normal text-slate-400">Question 6 - 7 • 10 minutes</h2>
-                            </div>
-                            <ChevronRight
-                                className="hidden lg:flex ml-auto size-10 opacity-0 group-hover:opacity-100 group-hover:translate-x-2 transition-all  duration-200"
-                            />
+            <div className="lg:border-2 lg:rounded-md p-5">
+                <h1 className="text-2xl font-bold">Pick a part</h1>
+                <h2 className="text-xl font-normal text-slate-400">Select the TOEIC writing part you want to practice:</h2>
+                <div className="w-full mt-4 flex flex-col gap-2">
+                    <div
+                        className="w-full p-5 flex flex-row items-center gap-5 border-2 rounded-md hover:bg-green-300/20 hover:border-green-300 transition-all duration-200 group cursor-pointer"
+                        onClick={() => setPart(2)}
+                    >
+                        <div className="p-3 rounded-full bg-green-100 text-green-600 group-hover:bg-green-200 transition-colors">
+                            <Mail className="size-10" />
                         </div>
-                        <div
-                            className="w-full p-5 flex flex-row items-center gap-5 border-2 rounded-md hover:bg-blue-300/20 hover:border-blue-300 transition-all duration-200 group cursor-pointer"
-                            onClick={() => setPart(3)}
-                        >
-                            <div className="p-3 rounded-full bg-blue-100 text-blue-600 group-hover:bg-blue-200 transition-colors">
-                                <NotebookText className="size-10" />
-                            </div>
-                            <div>
-                                <h1 className="text-xl font-bold group-hover:text-blue-800 transition-all duration-200">Part 3</h1>
-                                <h2 className="text-lg font-normal text-slate-400">Question 8 • 30 minutes</h2>
-                            </div>
-                            <ChevronRight
-                                className="hidden lg:flex ml-auto size-10 opacity-0 group-hover:opacity-100 group-hover:translate-x-2 transition-all duration-200"
-                            />
+                        <div>
+                            <h1 className="text-xl font-bold group-hover:text-green-800 transition-all duration-200">Part 2</h1>
+                            <h2 className="text-lg font-normal text-slate-400">Question 6 - 7 • 10 minutes</h2>
                         </div>
+                        <ChevronRight
+                            className="hidden lg:flex ml-auto size-10 opacity-0 group-hover:opacity-100 group-hover:translate-x-2 transition-all  duration-200"
+                        />
+                    </div>
+                    <div
+                        className="w-full p-5 flex flex-row items-center gap-5 border-2 rounded-md hover:bg-blue-300/20 hover:border-blue-300 transition-all duration-200 group cursor-pointer"
+                        onClick={() => setPart(3)}
+                    >
+                        <div className="p-3 rounded-full bg-blue-100 text-blue-600 group-hover:bg-blue-200 transition-colors">
+                            <NotebookText className="size-10" />
+                        </div>
+                        <div>
+                            <h1 className="text-xl font-bold group-hover:text-blue-800 transition-all duration-200">Part 3</h1>
+                            <h2 className="text-lg font-normal text-slate-400">Question 8 • 30 minutes</h2>
+                        </div>
+                        <ChevronRight
+                            className="hidden lg:flex ml-auto size-10 opacity-0 group-hover:opacity-100 group-hover:translate-x-2 transition-all duration-200"
+                        />
                     </div>
                 </div>
             </div>
-    }</div>;
+        </div>
+    </div>;
 }
 
 export default Generate;
--- frontend/src/components/Topic/Submit.tsx
+++ frontend/src/components/Topic/Submit.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { error } from "../Toast";
 import { useNavigate } from "react-router";
-import { Check, ChevronLeft, Clock } from "lucide-react";
+import { Bug, Check, ChevronLeft, Clock, Sparkle } from "lucide-react";
 import { BarLoader } from "react-spinners";
 import Markdown from "react-markdown";
 
@@ -13,15 +13,20 @@ function Submit({ topicId: id, preloadedData }: { topicId: string, preloadedData
     const [topic, setTopic] = useState<Topic | undefined>(preloadedData);
     const [text, setText] = useState<string>("");
     const [timeLeft, setTimeLeft] = useState<number>(0);
-    const timer = useRef<any>(null);
     const [confirmed, setConfirm] = useState<boolean>(false);
-    const [status, setStatus] = useState<"sending" | "sent">();
+    const [submissionStatus, setSubmissionStatus] = useState<"sending" | "sent">();
+    const topicReloadTimer = useRef<any>(null);
+    const submissionTimer = useRef<any>(null);
 
     const getTopic = useCallback(async () => {
         try {
             const response = await api.get<Topic>(`/topic?id=${id}`);
             setTopic(response.data);
-            setTimeLeft((response.data.part == "2" ? 10 : 30) * 60);
+            if (response.data.status != "pending") {
+                if (response.data.status == "done")
+                    setTimeLeft((response.data.part == "2" ? 10 : 30) * 60);
+                clearInterval(topicReloadTimer.current);
+            }
         } catch (err) {
             if (axios.isAxiosError(err) && err.status == 404) {
                 error("Topic not found >:(");
@@ -31,26 +36,30 @@ function Submit({ topicId: id, preloadedData }: { topicId: string, preloadedData
         }
     }, [id, navigator]);
     useEffect(() => {
-        if (!topic) void getTopic();
-        else setTimeLeft((topic.part == "2" ? 10 : 30) * 60); // Pre-loaded
-    }, [topic, getTopic]);
+        if (preloadedData && preloadedData.status == "done")
+            return void setTimeLeft((preloadedData.part == "2" ? 10 : 30) * 60);
+        if (topic && topic.status != "pending")
+            return;
+        topicReloadTimer.current = setInterval(() => void getTopic(), 1000);
+        return () => clearInterval(topicReloadTimer.current);
+    }, [preloadedData, topic, getTopic]);
     useEffect(() => {
-        timer.current = setInterval(
+        submissionTimer.current = setInterval(
             () => timeLeft > 0 && confirmed
                 ? setTimeLeft(timeLeft => timeLeft -= 1)
                 : undefined,
             1000
         );
-        return () => clearInterval(timer.current);
+        return () => clearInterval(submissionTimer.current);
     }, [timeLeft, confirmed]);
 
     const send = useCallback(async (text: string) => {
         if (!topic) return;
-        setStatus("sending");
+        setSubmissionStatus("sending");
         try {
             const response = await api.post<Submission>(`/submission?topic_id=${topic.id}`, { submission: text });
             await api.post(`/review?submission_id=${response.data.id}`);
-            setStatus("sent");
+            setSubmissionStatus("sent");
             setTimeout(() => navigator(`/topic/${topic.id}/submission/${response.data.id}`));
         } catch (err) {
             console.error(err);
@@ -64,85 +73,112 @@ function Submit({ topicId: id, preloadedData }: { topicId: string, preloadedData
                 <BarLoader width={300} />
             </div>
         </div>
-        : !confirmed
-            ? <div className="lg:w-2/3 flex flex-col text-lg my-auto mx-10 lg:m-auto p-5 border-2 rounded-md">
-                <h1 className="text-xl font-bold">Direction</h1>
-                {topic.part == "2"
-                    ? <p>
-                        In this part of the test, you will show how well you can write a response to an e-mail.<br />
-                        Your response will be scored on the quality and variety of your sentences, vocabulary, and organization.<br />
-                        You will have 10 minutes to read and answer each e-mail.
-                    </p>
-                    : <p>
-                        In this part of the test, you will write an essay in response to a question that asks you to state, explain, and support your opinion on an issue. Typically, an effective essay will contain a minimum of 300 words.<br />
-                        Your response will be scored on whether your opinion is supported with reasons and/or examples, grammar, vocabulary, and organization.<br />
-                        You will have 30 minutes to plan, write, and revise your essay.
-                    </p>
-                }
-                <button
-                    className="ml-auto mr-5 mt-5 p-2 border-2 rounded-md cursor-pointer"
-                    onClick={() => setConfirm(true)}
-                >Start writing</button>
-            </div>
-            : status
-                ? <div className="m-auto">{status == "sending"
-                    ? <div className="flex flex-col items-center gap-5">
-                        <h1 className="text-3xl font-bold">Sending submission</h1>
+        : topic.status != "done"
+            ? topic.status == "pending"
+                ? <div className="m-auto">
+                    <div className="flex flex-col items-center gap-5">
+                        <div className="relative">
+                            <div className="absolute inset-0 bg-blue-100 rounded-full animate-ping opacity-75"></div>
+                            <div className="relative bg-white p-6 rounded-full shadow-lg border ">
+                                <Sparkle className="w-10 h-10 text-blue-500 animate-pulse" />
+                            </div>
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <h1 className="text-3xl font-bold">Generating topic</h1>
+                            <p className="px-10 text-center lg:p-0 text-xl">The AI is crafting a unique prompt based on TOEIC standards</p>
+                        </div>
                         <BarLoader width={300} />
                     </div>
-                    : <div className="flex flex-col items-center gap-5">
-                        <div className="rounded-full p-2 text-white bg-green-600"><Check strokeWidth={3} className="size-15" /></div>
-                        <div className="flex flex-col items-center gap-2">
-                            <h1 className="text-3xl font-bold">Sent submission</h1>
-                            <p className="text-xl">Redirect to review page after 5s</p>
-                        </div>
+                </div>
+                : <div className="m-auto flex flex-col items-center gap-5">
+                    <div className="bg-red-300 p-6 rounded-full shadow-lg border ">
+                        <Bug className="w-10 h-10 text-red-500 animate-pulse" />
                     </div>
-                }</div>
-                : <div className="lg:w-9/10 h-full lg:overflow-hidden flex-1 flex flex-col gap-5 py-5 lg:py-10">
-                    <div
-                        className="w-fit flex flex-row items-center text-slate-400 hover:text-slate-800 cursor-pointer transition-all duration-200"
-                        onClick={() => navigator(`/topic/${topic.id}`, { viewTransition: true })}
-                    >
-                        <ChevronLeft className="size-7" />
-                        <p className="text-lg">Cancel & Go back</p>
-                    </div>
-                    <div className="flex flex-col lg:flex-row lg:gap-2 flex-1 min-h-0">
-                        <div className="lg:w-2/5 flex flex-col p-5 border-2 lg:rounded-md gap-2 ">
-                            <div className="flex flex-row items-center gap-5 ">
-                                {topic.part == "2"
-                                    ? <h1 className="w-fit p-2 bg-green-200 rounded-sm text-green-700 text-sm font-bold uppercase">Response to an email</h1>
-                                    : <h1 className="w-fit p-2 bg-blue-200 rounded-sm text-blue-700 text-sm font-bold uppercase">Opinion essay</h1>
-                                }
-                                <div className="flex flex-row items-center gap-2 text-slate-600">
-                                    <Clock className="size-7" />
-                                    <p className="font-semibold text-xl">{Math.floor(timeLeft / 60)}:{timeLeft % 60 < 10 ? "0" : ""}{timeLeft % 60}</p>
-                                </div>
-                            </div>
-                            <h1 className="text-2xl font-bold">{topic.summary?.summary}</h1>
-                            <div className="text-xl whitespace-pre-wrap overflow-y-auto"><Markdown>{topic.question}</Markdown></div>
-                        </div>
-                        <div className="lg:w-3/5 flex flex-col border-2 lg:rounded-md">
-                            <textarea
-                                className="flex-1 w-full p-6 resize-none focus:outline-none text-slate-800 leading-relaxed overflow-scroll"
-                                placeholder="Start writing your essay here..."
-                                value={text}
-                                onChange={(e) => setText(e.target.value)}
-                                spellCheck="false"
-                                rows={10}
-                            />
-                            <div className="py-5 px-10 border-t-2 flex flex-row items-center">
-                                <p className="text-slate-500">{text.split(" ").filter(x => x.length).length} word(s)</p>
-                                <button
-                                    className="ml-auto px-3 py-2 rounded-md text-white disabled:bg-blue-200 disabled:cursor-not-allowed enabled:bg-blue-600 enabled:cursor-pointer"
-                                    // disabled={topic.part == "3" && text.split(" ").filter(x => x.length).length < 250}
-                                    onClick={() => send(text)}
-                                >
-                                    Submit for review
-                                </button>
-                            </div>
-                        </div>
+                    <div className="flex flex-col items-center">
+                        <h1 className="text-3xl font-bold">Failed to generate topic</h1>
+                        <p className="text-xl px-10 text-center lg:p-0">There is an error occured while generating topic</p>
+                        <p className="text-xl text-center lg:p-0">Please look at the server console and try again</p>
                     </div>
                 </div>
+            : !confirmed
+                ? <div className="lg:w-2/3 flex flex-col text-lg my-auto mx-10 lg:m-auto p-5 border-2 rounded-md">
+                    <h1 className="text-xl font-bold">Direction</h1>
+                    {topic.part == "2"
+                        ? <p>
+                            In this part of the test, you will show how well you can write a response to an e-mail.<br />
+                            Your response will be scored on the quality and variety of your sentences, vocabulary, and organization.<br />
+                            You will have 10 minutes to read and answer each e-mail.
+                        </p>
+                        : <p>
+                            In this part of the test, you will write an essay in response to a question that asks you to state, explain, and support your opinion on an issue. Typically, an effective essay will contain a minimum of 300 words.<br />
+                            Your response will be scored on whether your opinion is supported with reasons and/or examples, grammar, vocabulary, and organization.<br />
+                            You will have 30 minutes to plan, write, and revise your essay.
+                        </p>
+                    }
+                    <button
+                        className="ml-auto mr-5 mt-5 p-2 border-2 rounded-md cursor-pointer"
+                        onClick={() => setConfirm(true)}
+                    >Start writing</button>
+                </div>
+                : submissionStatus
+                    ? <div className="m-auto">{submissionStatus == "sending"
+                        ? <div className="flex flex-col items-center gap-5">
+                            <h1 className="text-3xl font-bold">Sending submission</h1>
+                            <BarLoader width={300} />
+                        </div>
+                        : <div className="flex flex-col items-center gap-5">
+                            <div className="rounded-full p-2 text-white bg-green-600"><Check strokeWidth={3} className="size-15" /></div>
+                            <div className="flex flex-col items-center gap-2">
+                                <h1 className="text-3xl font-bold">Sent submission</h1>
+                                <p className="text-xl">Redirect to review page after 5s</p>
+                            </div>
+                        </div>
+                    }</div>
+                    : <div className="lg:w-9/10 h-full lg:overflow-hidden flex-1 flex flex-col gap-5 py-5 lg:py-10">
+                        <div
+                            className="w-fit flex flex-row items-center text-slate-400 hover:text-slate-800 cursor-pointer transition-all duration-200"
+                            onClick={() => navigator(`/topic/${topic.id}`, { viewTransition: true })}
+                        >
+                            <ChevronLeft className="size-7" />
+                            <p className="text-lg">Cancel & Go back</p>
+                        </div>
+                        <div className="flex flex-col lg:flex-row lg:gap-2 flex-1 min-h-0">
+                            <div className="lg:w-2/5 flex flex-col p-5 border-2 lg:rounded-md gap-2 ">
+                                <div className="flex flex-row items-center gap-5 ">
+                                    {topic.part == "2"
+                                        ? <h1 className="w-fit p-2 bg-green-200 rounded-sm text-green-700 text-sm font-bold uppercase">Response to an email</h1>
+                                        : <h1 className="w-fit p-2 bg-blue-200 rounded-sm text-blue-700 text-sm font-bold uppercase">Opinion essay</h1>
+                                    }
+                                    <div className="flex flex-row items-center gap-2 text-slate-600">
+                                        <Clock className="size-7" />
+                                        <p className="font-semibold text-xl">{Math.floor(timeLeft / 60)}:{timeLeft % 60 < 10 ? "0" : ""}{timeLeft % 60}</p>
+                                    </div>
+                                </div>
+                                <h1 className="text-2xl font-bold">{topic.summary?.summary}</h1>
+                                <div className="text-xl whitespace-pre-wrap overflow-y-auto"><Markdown>{topic.question}</Markdown></div>
+                            </div>
+                            <div className="lg:w-3/5 flex flex-col border-2 lg:rounded-md">
+                                <textarea
+                                    className="flex-1 w-full p-6 resize-none focus:outline-none text-slate-800 leading-relaxed overflow-scroll"
+                                    placeholder="Start writing your essay here..."
+                                    value={text}
+                                    onChange={(e) => setText(e.target.value)}
+                                    spellCheck="false"
+                                    rows={10}
+                                />
+                                <div className="py-5 px-10 border-t-2 flex flex-row items-center">
+                                    <p className="text-slate-500">{text.split(" ").filter(x => x.length).length} word(s)</p>
+                                    <button
+                                        className="ml-auto px-3 py-2 rounded-md text-white disabled:bg-blue-200 disabled:cursor-not-allowed enabled:bg-blue-600 enabled:cursor-pointer"
+                                        // disabled={topic.part == "3" && text.split(" ").filter(x => x.length).length < 250}
+                                        onClick={() => send(text)}
+                                    >
+                                        Submit for review
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
     }</div>;
 }
 
