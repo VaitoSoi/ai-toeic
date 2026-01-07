02d3f809b3d485f106cee36f85d1aa4d90c00cd9
VaitoSoi <duyduy15456@gmail.com>
2026-01-07 16:01:08 +0700

    feat(be): add route handler for p1 submission

--- backend/route/submission.py
+++ backend/route/submission.py
@@ -1,12 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from pydantic import BaseModel
 
 from lib.db import (
+    P1SubmitBody,
+    SlicedSubmission,
     delete_submission,
     get_submission,
     get_submissions,
-    submit,
-    update_submission,
+    p1_submit,
+    p23_submit,
+    # update_submission,
 )
 from lib.response import exception_handler
 
@@ -33,13 +36,29 @@ async def api_get_submission(id: str):
 
 @route.post("", description="Submit a submission", response_model=SlicedSubmission)
 @exception_handler
-async def api_submit(topic_id: str, body: SubmitBody):
-    return await submit(topic_id=topic_id, submitted_text=body.submission)
+async def api_submit(part: str, topic_id: str, body: SubmitBody | list[P1SubmitBody]):
+    if part == "1":
+        if not isinstance(body, list) or not isinstance(body[0], P1SubmitBody):
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, detail={"message": "wrong body"}
+            )
+        return await p1_submit(topic_id=topic_id, submissions=body)
+    elif part in ["2", "3"]:
+        if not isinstance(body, SubmitBody):
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, detail={"message": "wrong body"}
+            )
+        return await p23_submit(topic_id=topic_id, submitted_text=body.submission)
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail={"message": "wrong part"}
+        )
 
-@route.put("", description="Update a submission")
-@exception_handler
-async def api_update_submission(id: str, body: SubmitBody):
-    return await update_submission(id=id, submitted_text=body.submission)
+
+# @route.put("", description="Update a submission")
+# @exception_handler
+# async def api_update_submission(id: str, body: SubmitBody):
+#     return await update_submission(id=id, submitted_text=body.submission)
 
 
 @route.delete("", description="Delete a submission")
