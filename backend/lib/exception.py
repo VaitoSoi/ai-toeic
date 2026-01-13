56094a4730ca54d1f406fe4a993572425572a688
VaitoSoi <duyduy15456@gmail.com>
2026-01-13 07:30:32 +0700

    feat(be): define id field

--- backend/lib/exception.py
+++ backend/lib/exception.py
@@ -1,23 +1,34 @@
+from typing import Optional
+
+
 class TopicNotFound(ValueError): 
-    def __init__(self, id: str | None = None):
+    id: Optional[str] = None
+
+    def __init__(self, id: Optional[str] = None):
         super()
         self.message = "topic not found"
         self.id = id
 
 class QuestionNotFound(ValueError): 
-    def __init__(self, id: str | None = None):
+    id: Optional[str] = None
+    
+    def __init__(self, id: Optional[str] = None):
         super()
         self.message = "question not found"
         self.id = id
 
 class SubmissionNotFound(ValueError): 
-    def __init__(self, id: str | None = None):
+    id: Optional[str] = None
+    
+    def __init__(self, id: Optional[str] = None):
         super()
         self.message = "submission not found"
         self.id = id
 
 class ReviewNotFound(ValueError): 
-    def __init__(self, id: str | None = None):
+    id: Optional[str] = None
+    
+    def __init__(self, id: Optional[str] = None):
         super()
         self.message = "review not found"
         self.id = id
