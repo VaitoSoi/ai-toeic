1432595f8658b4faba2d2799209ddd658b775e2b
VaitoSoi <duyduy15456@gmail.com>
2026-01-25 00:45:44 +0700

    chore(fe): ignore pnp files

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-    globalIgnores(['dist', '.yarn', 'src/components/ui', 'src/api']),
+    globalIgnores(['dist', '.yarn', 'src/components/ui', 'src/api', '.pnp.*']),
     {
         files: ['**/*.{ts,tsx}'],
         extends: [
